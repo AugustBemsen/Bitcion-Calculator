bf69961c6400dad564b24b9eaa6b538462453e0d
Daniel Bemsen <danireptor7@gmail.com>
2020-04-11 14:33:06 +0100

    Modified the output and added input authentication

--- script.js
+++ script.js
@@ -3,35 +3,48 @@ button.addEventListener('click', () => {
   // 1. Select and read from the UI
     const priceBought = document.querySelector('.price__input--bought').value;
     const priceNow = document.querySelector('.price__input--now').value;
-    const investment = document.querySelector('.investment__btc').value;   
+    const investment = document.querySelector('.investment__btc').value;
+    const result = document.querySelector('.result'); 
 
-  // 2. Calculate the Profits
-    
-    const moneySpent = investment * priceBought;
-    const btcToday = priceNow * investment;
-    const btcProfit = btcToday - moneySpent;
-    const percentage = Math.round(btcProfit / btcToday * 100);
-    
-    console.log('Money spent ' + moneySpent);
-    console.log('Current Value ' + btcToday);
-    console.log('Profit or Loss ' + btcProfit);
-    console.log('percent ' + percentage);
-    
-  // 3. Write to the UI
-  const result = document.querySelector('.result');
-  if (btcProfit > 0 ) {
-    result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is ${priceNow}$/btc. You profit ${btcProfit}$. Youve made ${percentage}% Profit`;
 
-  } else if (btcProfit < 0) {
-    result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is ${priceNow}$/btc. You Lost ${btcProfit}$. Youve made ${percentage}% Loss`;
+  if (!priceBought || !priceNow || !investment) {
 
+    result.textContent = 'Pls fill out all fields';
   } else {
-    result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is still ${priceNow}$/btc. You niether loss nor gain`;
-
+        // 2. Calculate the Profits
+        const moneySpent = investment * priceBought;
+        const btcToday = priceNow * investment;
+        const btcProfit = btcToday - moneySpent;
+        const percentage = Math.round(btcProfit / btcToday * 100);
+          
+      // 3. Write to the UI
+      if (btcProfit > 0 ) {
+        result.classList.add('middle');
+        result.classList.add('loss');
+        result.classList.remove('middle');
+        result.classList.remove('loss');
+        result.classList.add('profit');
+        result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is ${priceNow}$/btc. You profit ${btcProfit}$. Youve made ${percentage}% Profit`;
+    
+      } else if (btcProfit < 0) {
+        result.classList.add('middle');
+        result.classList.add('profit');
+        result.classList.remove('middle');
+        result.classList.remove('profit');
+        result.classList.add('loss');
+        result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is ${priceNow}$/btc. You Lost ${btcProfit}$. Youve made ${percentage}% Loss`;
+    
+      } else {
+        result.classList.add('profit');
+        result.classList.add('loss');
+        result.classList.remove('profit');
+        result.classList.remove('loss');
+        result.classList.add('middle');
+        result.textContent = `You bought ${investment}btc at ${moneySpent}$ when the price was ${priceBought}$/btc. The current price is still ${priceNow}$/btc. You niether loss nor gain`;
+    
+      }
+    
   }
-
+  
 })
 
-// Things to do tomorrow!
-// 1. Add colors to the output;
-// 2. check for validations;
